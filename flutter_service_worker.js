d47e9e7d5292eefccab3199337d792ba9e34ef00
= <=>
2021-07-12 19:17:43 +0430

    Fixed srcs for web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "a343d01e8cc8e460d0526fb92f70b5f9",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+  "AimBoosterFlutter/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"AimBoosterFlutter/assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"AimBoosterFlutter/assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"AimBoosterFlutter/assets/NOTICES": "a343d01e8cc8e460d0526fb92f70b5f9",
+"AimBoosterFlutter/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"AimBoosterFlutter/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"AimBoosterFlutter/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "03704ef96f756822aa40ac42e72dff30",
 "/": "03704ef96f756822aa40ac42e72dff30",
 "main.dart.js": "3b1df1871a72775a284d7a3a72fc322a",
